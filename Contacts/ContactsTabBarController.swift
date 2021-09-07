f84e286c2ba8a10739541217087d5318d8c1f648
Igor Buzykin <lexemz@gmail.com>
2021-09-07 15:55:18 +0300

    tab bar vc creating

---
+++ Contacts/ContactsTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  ContactsTabBarController.swift
+//  Contacts
+//
+//  Created by Igor on 07.09.2021.
+//
+
+import UIKit
+
+class ContactsTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
