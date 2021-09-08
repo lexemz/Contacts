4883ba0d196521d93c35eefaf936ee73600f6c8f
Igor Buzykin <lexemz@gmail.com>
2021-09-08 21:48:06 +0300

    clear contact list file

--- Contacts/ViewControllers/ContactListViewController.swift
+++ Contacts/ViewControllers/ContactListViewController.swift
@@ -8,25 +8,11 @@
 import UIKit
 
 class ContactListViewController: UITableViewController {
-    
     var contacts: [Person]!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
 
     // MARK: - Table view data source
 
-
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return contacts.count
     }
 
@@ -34,22 +20,19 @@ class ContactListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
         var content = cell.defaultContentConfiguration()
         let contact = contacts[indexPath.row]
-        
+
         content.text = contact.getNameAndSurname
-        
+
         cell.contentConfiguration = content
         return cell
     }
 
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         guard let detailInfoVC = segue.destination as? DetailContactInfoViewController else { return }
         guard let indexPath = tableView.indexPathForSelectedRow else { return }
-        
+
         detailInfoVC.contactInfo = contacts[indexPath.row]
     }
-
 }
