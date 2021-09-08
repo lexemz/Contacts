ffd5a5d597eaebbd158f1157414313bb4269854d
Igor Buzykin <lexemz@gmail.com>
2021-09-08 21:46:30 +0300

    contactList

--- Contacts/ViewControllers/DetailContactInfoViewController.swift
+++ Contacts/ViewControllers/DetailContactInfoViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class DetailContactInfoViewController: UIViewController {
+    
+    @IBOutlet var phoneNumberLabel: UILabel!
+    @IBOutlet var mailLabel: UILabel!
+    
+    var contactInfo: Person!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateUI() {
+        title = contactInfo.getNameAndSurname
+        phoneNumberLabel.text = contactInfo.phoneNumber
+        mailLabel.text = contactInfo.mail
     }
-    */
-
 }
