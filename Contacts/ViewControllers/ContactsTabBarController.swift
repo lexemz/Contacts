a70c119f14eb16e9e5ad610d0ea2f48f9257fdee
Igor Buzykin <lexemz@gmail.com>
2021-09-08 21:46:10 +0300

    tapBar data pushing

--- Contacts/ViewControllers/ContactsTabBarController.swift
+++ Contacts/ViewControllers/ContactsTabBarController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class ContactsTabBarController: UITabBarController {
+    private let contasts = Person.getRandomPersons()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        pushDataOnViewControllers()
+
         // Do any additional setup after loading the view.
     }
-    
+
+    private func pushDataOnViewControllers() {
+        guard let viewControllers = viewControllers else { return }
+
+        for vc in viewControllers {
+            guard let nc = vc as? UINavigationController else { return }
+
+            if let contactListVC = nc.topViewController as? ContactListViewController {
+                contactListVC.contacts = contasts
+            } else if let gContactListVC = nc.topViewController as? GroupedContactListViewController {
+                gContactListVC.contacts = contasts
+            }
+        }
+    }
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
 }
