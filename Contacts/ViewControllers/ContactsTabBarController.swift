bcb4b26134419ef96492c60ceffd4021d8a9a957
Igor Buzykin <lexemz@gmail.com>
2021-09-08 21:48:27 +0300

    clear contactListtapBar

--- Contacts/ViewControllers/ContactsTabBarController.swift
+++ Contacts/ViewControllers/ContactsTabBarController.swift
@@ -14,8 +14,6 @@ class ContactsTabBarController: UITabBarController {
         super.viewDidLoad()
 
         pushDataOnViewControllers()
-
-        // Do any additional setup after loading the view.
     }
 
     private func pushDataOnViewControllers() {
@@ -31,14 +29,4 @@ class ContactsTabBarController: UITabBarController {
             }
         }
     }
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
 }
